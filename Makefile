04ffc851bb4f2df79b5e2de226339e405eb004de
Shawn Parker <shawn@topfroggraphics.com>
2020-12-31 20:20:33 -0800

    Speed up slogan load
    Immediately query but wait for DOMContentLoaded to insert the fetched
    slogan.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: local-server
 
-host = "localhost"
+host = "0.0.0.0"
 port = "8080"
 
 local-server:
--- js/sloganator.js
+++ js/sloganator.js
@@ -1,98 +1,98 @@
 const sloganatorHtml = 
 '<div id="sloganator">' +
-	'<span class="slogan" title=""></span>' +
-	'<span class="sloganator-form" style="display: none;">' +
-		'<form>' +
-			'<input name="slogan" type="text">' +
-			'<button class="slogan-submit" disabled><span>Add Slogan</span></button>' +
-			'<button class="slogan-cancel">&times;<span>Cancel</span></button>' +
-			'<a href="/mies/sloganator">Sloganator <i class="fa fa-external-link-square"></a></a>' +
-		'</form>' +
-	'</span>' +
+    '<span class="slogan" title=""></span>' +
+    '<span class="sloganator-form" style="display: none;">' +
+        '<form>' +
+            '<input name="slogan" type="text">' +
+            '<button class="slogan-submit" disabled><span>Add Slogan</span></button>' +
+            '<button class="slogan-cancel">&times;<span>Cancel</span></button>' +
+            '<a href="/mies/sloganator">Sloganator <i class="fa fa-external-link-square"></a></a>' +
+        '</form>' +
+    '</span>' +
 '</div>';
 
 function loadTemplate() {
-	let template = document.createElement("template");
-	let html = sloganatorHtml.trim();
-	
-	template.innerHTML = html;
-	
-	return template.content.firstChild;
+    let template = document.createElement("template");
+    let html = sloganatorHtml.trim();
+    
+    template.innerHTML = html;
+    
+    return template.content.firstChild;
 }
 
 function attachEventListeners(sloganator) {
-	let form = sloganator.querySelector(".sloganator-form");
-	let submitButton = form.querySelector(".slogan-submit");
+    let form = sloganator.querySelector(".sloganator-form");
+    let submitButton = form.querySelector(".slogan-submit");
 
-	sloganator.querySelector(".slogan").addEventListener("click", (event) => {
-		event.preventDefault();
-		event.stopPropagation();
-		form.style.display = "";
-	    sloganator.classList.add("open");
+    sloganator.querySelector(".slogan").addEventListener("click", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        form.style.display = "";
+        sloganator.classList.add("open");
     }, false);
 
-	sloganator.querySelector("form input[name='slogan']").addEventListener("keyup", (event) => {
-		let _value = event.target.value;
-		submitButton.toggleAttribute("disabled", !_value.length);
-	}, false);
+    sloganator.querySelector("form input[name='slogan']").addEventListener("keyup", (event) => {
+        let _value = event.target.value;
+        submitButton.toggleAttribute("disabled", !_value.length);
+    }, false);
 
-	sloganator.querySelector("form .slogan-cancel").addEventListener("click", (event) => {
-		event.preventDefault();
-		event.stopPropagation();
-		form.style.display = "none";
+    sloganator.querySelector("form .slogan-cancel").addEventListener("click", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        form.style.display = "none";
         sloganator.classList.remove("open");
-	}, false);
+    }, false);
 
-	sloganator.querySelector("form").addEventListener("submit", (event) => {
-		event.preventDefault();
-		event.stopPropagation();
-		
-		let newSlogan = form.querySelector("input[name='slogan']").value.trim();
+    sloganator.querySelector("form").addEventListener("submit", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        
+        let newSlogan = form.querySelector("input[name='slogan']").value.trim();
 
-		if (!newSlogan.length) {
-			return false;
-		}		
-	
-		addSlogan(newSlogan);
-	}, false);
+        if (!newSlogan.length) {
+            return false;
+        }		
+    
+        addSlogan(newSlogan);
+    }, false);
 }
 
 function addSlogan(newSlogan) {
-	let url = new URL(window.location.origin + "/mies/sloganator/v1/slogans");	
-	let params = {
-		headers: {
-			"Accept": "application/json",
-			"Content-Type": "application/json"
-		},
-		method: 'POST',
-		body: JSON.stringify({"slogan": newSlogan})
-	}
-	fetch(url, params)
-		.then(res => res.json())
-		.then(res => writeSlogan(res));
+    let url = new URL(window.location.origin + "/mies/sloganator/v1/slogans");	
+    let params = {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify({"slogan": newSlogan})
+    }
+    fetch(url, params)
+        .then(res => res.json())
+        .then(res => writeSlogan(res));
 }
 
 function clearStage() {
-	let sloganator = document.querySelector('#sloganator');
-	if (sloganator) {
-		sloganator.remove();
-	}
+    let sloganator = document.querySelector('#sloganator');
+    if (sloganator) {
+        sloganator.remove();
+    }
 }
 
 function writeSlogan(slogan) {
-	clearStage();
-	let sloganator = loadTemplate();
+    clearStage();
+    let sloganator = loadTemplate();
     let sl = sloganator.querySelector(".slogan");
-	sl.innerText = slogan.slogan;
-	sl.title = "Authored By: " + slogan.username;
+    sl.innerText = slogan.slogan;
+    sl.title = "Authored By: " + slogan.username;
 
     attachEventListeners(sloganator);
 
-	let wrapper = document.querySelector('#logo .wrapper');
-	wrapper.appendChild(sloganator);
+    let wrapper = document.querySelector('#logo .wrapper');
+    wrapper.appendChild(sloganator);
 }
 
-function getSlogan(pageNum = 1) {
+function getSlogan() {
     let url = new URL(window.location.origin + "/mies/sloganator/v1/slogans");
     url.search = new URLSearchParams({
         page: 1,
@@ -107,9 +107,12 @@ function getSlogan(pageNum = 1) {
 
     fetch(url, params)
         .then(res => res.json())
-        .then(res => writeSlogan(res.slogans[0]));
+        .then(res => {
+            let slogan = res.slogans[0];
+            "loading" === document.readyState ?
+                document.addEventListener("DOMContentLoaded", (event) => { writeSlogan(slogan); }, false) :
+                writeSlogan(slogan);
+        });
 }
 
-"loading" === document.readyState ?
-    document.addEventListener("DOMContentLoaded", (event) => { getSlogan(); }, false) :
-    getSlogan();
+getSlogan();
