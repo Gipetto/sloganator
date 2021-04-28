ad1f5ce455f0228e44612b9b88879794963f9959
Shawn Parker <shawn@topfroggraphics.com>
2021-04-28 10:12:30 -0700

    Munge asset urls when accessing dev-server remotely

--- lib/local/router.php
+++ lib/local/router.php
@@ -3,7 +3,13 @@
 define("DOCROOT", dirname(dirname(__DIR__)) . "/lib/local/");
 
 if (preg_match("/\.(?:css|js)$/", $_SERVER["REQUEST_URI"])) {
-    return false;
+    if (preg_match("|^/mies|", $_SERVER["REQUEST_URI"]) {
+	    $path = preg_replace("|^/mies/|", "", $_SERVER["REQUEST_URI"]);
+        header("Content-Type: " . mime_content_type($path));
+        return readfile($path);
+	} else {
+	    return false;
+	}
 } elseif (preg_match("/local\/index\.html$/", $_SERVER["REQUEST_URI"])) {
     ob_start(); 
     include_once "lib/local/index.html";
