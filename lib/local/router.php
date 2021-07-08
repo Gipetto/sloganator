03d92501d4f4088bf8201bb1c430bed12cfc49b4
Shawn Parker <shawn@topfroggraphics.com>
2021-07-08 15:13:28 -0700

    Clean up local router
    Originally thought it would be parsing the local template, but there was
    no real need for it.

--- lib/local/router.php
+++ lib/local/router.php
@@ -13,11 +13,8 @@ if (preg_match("/\.(?:css|js)$/", $_SERVER["REQUEST_URI"])) {
         return false;
     }
 } elseif (preg_match("/local\/index\.html$/", $_SERVER["REQUEST_URI"])) {
-    ob_start(); 
-    include_once "lib/local/index.html";
-    $output = ob_get_clean();
     header("Content-Type: text/html");
-    echo $output;
+    return readfile("lib/local/index.html");
 } elseif (preg_match("/coverage.*/", $_SERVER["REQUEST_URI"])) {
     return false;
 } else {
