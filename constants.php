18a7902e9f4b44f513a44aa6115d36370efe9340
Shawn Parker <shawn@topfroggraphics.com>
2022-01-28 02:17:15 -0800

    Bump version

--- constants.php
+++ constants.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types = 1);
 
 define("BASEDIR", dirname(__FILE__));
-define("SLGNTR_VERSION", "1.1.1");
+define("SLGNTR_VERSION", "1.2.0");
 
 if (getenv("SLOGANATOR_LOCAL_DOCKER")) {
     define("DOCROOT", BASEDIR . "/_local/");
